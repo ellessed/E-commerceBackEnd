e0074db6a35674e7a61f86b8da346ea0ff10a3ab
elleserdonaldson <ellessedon@gmail.com>
2023-03-01 20:26:30 +0000

    deleted string

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,10 +9,7 @@ router.get("/", async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productInfo = await Product.findAll({
-      include: [
-        Category,
-        { model: Tag, through: ProductTag, as: "product-tag" },
-      ],
+      include: [Category, { model: Tag, through: ProductTag }],
     });
     res.status(200).json(productInfo);
   } catch (err) {
@@ -26,10 +23,7 @@ router.get("/:id", async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productInfo = await Product.findByPk(req.params.id, {
-      include: [
-        Category,
-        { model: Tag, through: ProductTag, as: "product-tag" },
-      ],
+      include: [Category, { model: Tag, through: ProductTag }],
     });
     if (!productInfo) {
       res
